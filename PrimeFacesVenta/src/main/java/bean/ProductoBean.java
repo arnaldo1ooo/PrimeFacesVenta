59fd4113c4f788098f30d64764293f1272192dfd
arnaldorcm <arnaldo1ooo95@gmail.com>
2021-07-02 19:50:43 -0400

    Se creó módulo de Producto

---
+++ PrimeFacesVenta/src/main/java/bean/ProductoBean.java
@@ -0,0 +1,134 @@
+package bean;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import dao.ProductoDAO;
+import model.Producto;
+
+@ManagedBean
+@ViewScoped
+public class ProductoBean {
+	// Inicializar variables
+	private Producto producto = new Producto();
+	private List<Producto> lstProductos;
+	private String accion;
+
+	// Getters y Setters
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public String getAccion() {
+		return accion;
+	}
+
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public List<Producto> getLstProductos() {
+		return lstProductos;
+	}
+
+	public void setLstProductos(List<Producto> lstProductos) {
+		this.lstProductos = lstProductos;
+	}
+
+	// Metodos
+	public void listar() throws Exception {
+		ProductoDAO dao;
+		try {
+			dao = new ProductoDAO();
+			lstProductos = dao.listar();
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+
+	public void leerID(Producto per) throws Exception {
+		ProductoDAO dao;
+		Producto temp;
+
+		try {
+			dao = new ProductoDAO();
+			dao.leerID(per);
+			temp = dao.leerID(per);
+
+			if (temp != null) {
+				this.producto = temp;
+				this.accion = "Modificar";
+			}
+
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public void operar() throws Exception {
+		switch (accion) {
+		
+		case "Registrar":
+			this.registrar();
+			this.limpiar();
+			break;
+
+		case "Modificar":
+			this.modificar();
+			this.limpiar();
+			break;
+
+		default:
+			throw new IllegalArgumentException("Unexpected value switch: " + accion);
+		}
+	}
+
+	private void registrar() throws Exception {
+		ProductoDAO dao;
+		try {
+			dao = new ProductoDAO();
+			dao.registrar(producto);
+			this.listar();
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	private void modificar() throws Exception {
+		ProductoDAO dao;
+		try {
+			dao = new ProductoDAO();
+			dao.modificar(producto);
+			this.listar();
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public void eliminarID(Producto per) throws Exception {
+		ProductoDAO dao;
+
+		try {
+			dao = new ProductoDAO();
+			dao.eliminarID(per);
+			this.listar();
+
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	public void limpiar() {
+		this.producto.setCodigo(0);
+		this.producto.setNombre("");
+		this.producto.setPrecio(0);
+		
+		System.out.println("Se limpio campos");
+		
+	}
+}
